5f7bae660813921b1f3691fe02ec2584e11ae3a9
UshurbakiyevDavlat <dushurbakiev@gmail.com>
2022-05-29 15:53:17 +0600

    added changes to main blade style

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -5,19 +5,35 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }} ">
+    <title>Laravel</title>
 </head>
 <body>
 <div class="container">
     <div class="row">
-        <nav>
-            <ul>
-                <li><a href="{{ route('user.index') }}">Users</a></li>
-{{--                <li><a href="{{ route('post.index') }}">Posts</a></li>--}}
-{{--                <li><a href="{{ route('about.index') }}">About</a></li>--}}
-{{--                <li><a href="{{ route('contact.index') }}">Contacts</a></li>--}}
-            </ul>
+
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('user.index') }}">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </div>
 </div>
