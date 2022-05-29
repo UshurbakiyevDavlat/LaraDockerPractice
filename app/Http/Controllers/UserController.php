dbd64dc661fd297191eed801f275c031ef22a7e9
UshurbakiyevDavlat <dushurbakiev@gmail.com>
2022-05-29 16:50:16 +0600

    added users controller route group, exceptions to verify csrf, soft delete to user model and migration, logic to crud for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('components.users.data');
+    }
+
+    public function getUsers()
+    {
+        return User::all();
+    }
+
+    public function getUsersWithTrashed()
+    {
+        return User::withTrashed()->get();
+    }
+
+    public function getUser(Request $request)
+    {
+        $user = User::find($request->id);
+        return $user;
+    }
+
+    public function factory_create()
+    {
+        try {
+            $user = User::factory()->create();
+        } catch (\Exception $exception) {
+            return $exception->getMessage();
+        }
+        return response(['success' => 1, 'user' => $user]);
+    }
+
+    public function create(Request $data)
+    {
+        try {
+            $user = User::create($data->toArray());
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+        return $user;
+    }
+
+    public function edit(Request $changed_data)
+    {
+        try {
+            $user = User::find($changed_data->id);
+            $user->update($changed_data->toArray());
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+        return $user;
+    }
+
+    public function delete(Request $request)
+    {
+        try {
+            $user = User::find($request->id);
+            $user->delete();
+        } catch (\Exception $exception) {
+            return $exception->getMessage();
+        }
+        return response(['success' => 1]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,18 @@ Route::get('/', function () {
 });
 // Permission for new files or folders chmod -R 777 ./ && chmod -R 777 /folder path
 
-Route::get('/user',[UserController::class,'index'])->name('user.index');
-//Route::get('/post',"")->name('post.index');
-//Route::get('/about',"")->name('about.index');
-//Route::get('/contact',"")->name('contact.index');
+Route::controller(UserController::class)->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
+    Route::get('/user/getAll', [UserController::class, 'getUsers'])->name('user.show');
+    Route::get('/user/withTrash', [UserController::class, 'getUsersWithTrashed'])->name('user.withTrash');
+    Route::get('/user/get_by_id', [UserController::class, 'getUser'])->name('user.show_by_id');
+
+    Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
+    Route::get('/user/fake_create', [UserController::class, 'factory_create'])->name('user.fake_create');
+
+    Route::put('/user/edit', [UserController::class, 'edit'])->name('user.edit');
+
+    Route::delete('/user/delete', [UserController::class, 'delete'])->name('user.delete');
+
+});
