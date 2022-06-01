11aa867d41f2a15434665cedd6e13fbb95d6c31c
Davlat <dushurbakiev@gmail.com>
2022-06-01 20:18:07 +0600

    added auth functional

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::resources([
     'info' => InfoController::class,
     'post' => PostController::class,
 ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
