6a1b107d31423a49380d795620408ca79cb01bb0
Davlat <dushurbakiev@gmail.com>
2022-06-17 10:33:38 +0600

    added layout content and inclide side bar for admin.post.index, TODO: move all the functional to admin panel from custom views

--- resources/views/components/includes/admin/post/sidebar.blade.php
+++ resources/views/components/includes/admin/post/sidebar.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-header">EXAMPLES</li>
+        <li class="nav-item">
+            <a href="{{route('admin.post.index')}}" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                    Posts
+                    <span class="badge badge-info right">{{\App\Models\Post::count()}}</span>
+                </p>
+            </a>
+        </li>
+    </ul>
+</nav>
